12cfc51ffef7b779dce8cf63de45879ce18e6471
putuphillipsteven <putu.phillip@gmail.com>
2025-07-28 22:02:07 +0700

    dont know what i create

--- src/ui/landing-page/landing-page.tsx
+++ src/ui/landing-page/landing-page.tsx
@@ -1,26 +1,33 @@
-import { useTheme, VStack } from '@chakra-ui/react';
+import {Flex, Text, useTheme, VStack} from '@chakra-ui/react';
 import LandingPageNavBar from './component/landing-page-nav-bar';
-import { useState } from 'react';
+import {useState} from 'react';
 import {RootState} from "../../utils/redux/store";
-import { useSelector } from 'react-redux';
+import {useSelector} from 'react-redux';
 
 export default function LandingPage() {
-	const theme = useTheme();
-	const [sideNavDisplay, setSideNavDisplay] = useState(true);
-	const toggleSideNav = () => {
-		setSideNavDisplay(!sideNavDisplay);
-	};
+    const theme = useTheme();
+    const [sideNavDisplay, setSideNavDisplay] = useState(true);
+    const toggleSideNav = () => {
+        setSideNavDisplay(!sideNavDisplay);
+    };
 
-	const user = useSelector((state: RootState) => state.AuthReducer);
+    const user = useSelector((state: RootState) => state.AuthReducer);
 
-	console.log("user: ", user)
-	return (
-		<VStack
-			spacing={'1em'}
-			bgColor={theme.colors.bacgkround}
-			borderBottom={`2px solid ${theme.colors.primary}`}
-		>
-			<LandingPageNavBar toggleSideNavDisplay={toggleSideNav} />
-		</VStack>
-	);
+    console.log("user: ", user)
+    return (
+        <Flex w={"full"} flexDir={"column"} rowGap={".5em"}>
+            <VStack
+                w={"full"}
+                spacing={'1em'}
+                bgColor={theme.colors.bacgkround}
+                borderBottom={`2px solid ${theme.colors.primary}`}
+            >
+                <LandingPageNavBar toggleSideNavDisplay={toggleSideNav}/>
+            </VStack>
+            <Flex p={".5em"}>
+                <Text>Test</Text>
+            </Flex>
+        </Flex>
+
+    );
 }
