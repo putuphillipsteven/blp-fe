d86342036cef1e135e7e200dae089b2058190aea
putuphillipsteven <putu.phillip@gmail.com>
2025-11-28 23:32:56 +0700

    add padding

--- src/ui/landing-page/component/landing-page-nav-bar.tsx
+++ src/ui/landing-page/component/landing-page-nav-bar.tsx
@@ -24,7 +24,7 @@ export default function LandingPageNavBar({toggleSideNavDisplay}: NavBarProps) {
             alignItems={'center'}
             flexDir={{base: 'column'}}
             borderBottom={`2px solid ${theme.colors.primary}`}
-            px={{base: "0", md: "10em"}}
+            px={{base: ".5em", md: "10em"}}
         >
             <Flex
                 w={'100%'}
--- src/ui/landing-page/landing-page.tsx
+++ src/ui/landing-page/landing-page.tsx
@@ -36,7 +36,6 @@ export default function LandingPage() {
                     <OurLocation/>
                     <LandingPageFooter/>
                 </VStack>
-
             </VStack>
         </Container>
 
